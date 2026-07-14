34edd7f5156e52014ca536c44d8a49bec17f1705
tlovett2025-Apollo <tlovett2025@gmail.com>
2026-07-14 17:17:11 -0500

    Test candidate cooking methods serving styles and kid overlays

---
+++ test_candidate_ontology.py
@@ -0,0 +1,46 @@
+from recipe_engine import (
+    _experience_overlays,
+    _method_is_eligible,
+    _serving_styles,
+)
+
+
+def test_plate_and_bowl_are_serving_styles_not_cooking_methods():
+    assert _serving_styles("skillet") == ["plate", "bowl"]
+    assert _serving_styles("casserole") == ["plate", "bowl"]
+    assert _serving_styles("soup") == ["bowl", "cup"]
+
+
+def test_handheld_requires_a_wrapper_or_bread():
+    assert not _method_is_eligible("handheld", ["Chicken", "Mushrooms"], "", [])
+    assert _method_is_eligible("handheld", ["Chicken", "Tortillas"], "", [])
+    assert _method_is_eligible("handheld", ["Chicken", "Bread"], "", [])
+
+
+def test_soup_requires_a_liquid_path():
+    assert not _method_is_eligible("soup", ["Chicken", "Carrots"], "", [])
+    assert _method_is_eligible("soup", ["Chicken", "Chicken broth"], "", [])
+    assert _method_is_eligible("soup", ["Chicken", "Cream of chicken soup"], "", [])
+
+
+def test_casserole_requires_structure():
+    assert not _method_is_eligible("casserole", ["Chicken", "Mushrooms"], "", [])
+    assert _method_is_eligible("casserole", ["Chicken", "Mushrooms"], "Rice", [])
+    assert _method_is_eligible("casserole", ["Chicken", "Cheese"], "", [])
+
+
+def test_grill_requires_available_grill_equipment():
+    assert not _method_is_eligible("grill", ["Chicken"], "", ["Oven", "Stovetop"])
+    assert _method_is_eligible("grill", ["Chicken"], "", ["Outdoor Grill"])
+
+
+def test_kid_adventure_is_an_overlay_on_the_same_meal():
+    assert _experience_overlays(False) == []
+    overlays = _experience_overlays(True, "dinosaurs")
+    assert overlays == [{
+        "type": "kid_adventure",
+        "theme": "dinosaurs",
+        "same_meal": True,
+        "include_fun_facts": True,
+        "include_conversation_prompts": True,
+    }]
